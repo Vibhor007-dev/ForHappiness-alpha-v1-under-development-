bd75c4318cf153060aebb101d1a9de5fd7102cea
Vibhor Singh <78255056+Vibhor007-dev@users.noreply.github.com>
2021-11-16 17:22:32 +0530

    Add files via upload

---
+++ screens/Meme.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {View, StyleSheet, SafeAreaView, Text, Image, TouchableOpacity, ImageBackground, Platform} from 'react-native'
+
+export default class Meme extends React.Component{
+    render(){
+        return(
+            <View>
+            <Text>Meme Screen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }
+  });
